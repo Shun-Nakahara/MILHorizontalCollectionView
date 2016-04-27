8f0c422f757a6073786aaae29ac63d6637373f07
shun nakahara <naka419@gmail.com>
2016-04-27 11:31:20 +0900

    ライブラリを使えるように修正

--- MILHorizontalCollectionView/MILHorizontalCollectionView/MILHorizontalCollectionViewController.swift
+++ MILHorizontalCollectionView/MILHorizontalCollectionView/MILHorizontalCollectionViewController.swift
@@ -5,16 +5,16 @@ Licensed Materials - Property of IBM
 
 import UIKit
 
-class MILHorizontalCollectionViewController: UICollectionViewController {
+public class MILHorizontalCollectionViewController: UICollectionViewController {
 
     //here you can customize what kind of objects the dataArray would hold, in this case we used strings for imageURL's
-    var dataArray : [String] = []
+    public var dataArray : [String] = []
     
     //ideally the MILHorizontalCollectionView wouldn't have a imageCache itself, instead we would be caching to a global image cache so anywhere in the app we could access cached images
-    var imageCache : [String : UIImage] = [String : UIImage]()
+    public var imageCache : [String : UIImage] = [String : UIImage]()
     
     //Must set this string for placeHolder images to show while it waits for image URL's to download
-    var localPlaceHolderImageName = ""
+    public var localPlaceHolderImageName = ""
     
     //sets the MILHorizontalCollectionView to handle image URL strings with built in mechanisms to display data in the cell
     private var handleImageURLStrings = true
@@ -26,17 +26,25 @@ class MILHorizontalCollectionViewController: UICollectionViewController {
     private var handleLocalImageNameStrings = false
     
     
-    override func viewDidLoad() {
+    override public func viewDidLoad() {
         super.viewDidLoad()
 
         //initial set up of the collection view
         self.setUpCollectionView()
     }
     
+    override public func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+// MARK: Public func
+extension MILHorizontalCollectionViewController {
+    
     /**
-    This method sets up the collectionview to have n placeholder items while it waits for a call back from the server (when the refresh method is called)
-    */
-    func setUpWithInitialPlaceHolderItems(numberOfPlaceHolderCells : Int){
+     This method sets up the collectionview to have n placeholder items while it waits for a call back from the server (when the refresh method is called)
+     */
+    public func setUpWithInitialPlaceHolderItems(numberOfPlaceHolderCells : Int){
         
         for _ in 1..<numberOfPlaceHolderCells {
             self.dataArray.append("")
@@ -44,25 +52,57 @@ class MILHorizontalCollectionViewController: UICollectionViewController {
     }
     
     /**
-    This method is called when there has been data recieved from the server. It sets up the collectionview to handle this new data.
-    
-    - parameter newDataArray:
-    */
-    func refresh(newDataArray : [String]){
+     This method is called when there has been data recieved from the server. It sets up the collectionview to handle this new data.
+     
+     - parameter newDataArray:
+     */
+    public func refresh(newDataArray : [String]){
         if(newDataArray.count > 0){
             self.dataArray = newDataArray
             self.collectionView!.reloadData()
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
     
     /**
-    This method sets up the collectionView with various inital settings.
-    */
+     This method sets the MILHorizontalCollectionView to handle image URL strings with built in mechanisms to display data in the cell
+     */
+    public func setToHandleImageURLStrings(){
+        
+        self.handleLocalImageNameStrings = false
+        self.handleImageURLStrings = true
+        self.handleImageURLStringsUsingSDWebImage = false
+        
+    }
+    
+    /**
+     This method sets the MILHorizontalCollectionView to handle image URL strings with SDWebImage to display data in the cell
+     */
+    public func setToHandleImageURLStringsUsingSDWebImage(){
+        
+        self.handleLocalImageNameStrings = false
+        self.handleImageURLStrings = false
+        self.handleImageURLStringsUsingSDWebImage = true
+        
+    }
+    
+    /**
+     This method sets the MILHorizontalCollectionView to handle local image name strings with built in mechanisms to display data in the cell
+     */
+    public func setToHandleLocalImageNameStrings(){
+        
+        self.handleLocalImageNameStrings = true
+        self.handleImageURLStrings = false
+        self.handleImageURLStringsUsingSDWebImage = false
+        
+    }
+}
+
+// MARK: Privete func
+extension MILHorizontalCollectionViewController {
+
+    /**
+     This method sets up the collectionView with various inital settings.
+     */
     private func setUpCollectionView(){
         //hide horizontal scroll indicator
         self.collectionView!.showsHorizontalScrollIndicator = false;
@@ -74,101 +114,17 @@ class MILHorizontalCollectionViewController: UICollectionViewController {
         //set up the collection view with custom collection view nib
         let nib : UINib = UINib(nibName: "MILHorizontalCollectionViewCell", bundle:nil)
         self.collectionView!.registerNib(nib,
-            forCellWithReuseIdentifier: "horizontalcell");
+                                         forCellWithReuseIdentifier: "horizontalcell");
     }
-    
-    
+
     /**
-     This method sets the MILHorizontalCollectionView to handle image URL strings with built in mechanisms to display data in the cell
+     This method sets up the MILHorizontalCollectionViewCell with a locally stored image with respect to indexPath.row in the dataArray. This method can be used when it is assumed that the strings the data array holds represent the names of locally stored images in Images.xcassets
+     
+     - parameter cell:
+     - parameter indexPath:
+     
+     - returns: MILHorizontalCollectionViewCell
      */
-    func setToHandleImageURLStrings(){
-        
-        self.handleLocalImageNameStrings = false
-        self.handleImageURLStrings = true
-        self.handleImageURLStringsUsingSDWebImage = false
-        
-    }
-    
-    
-    /**
-     This method sets the MILHorizontalCollectionView to handle image URL strings with SDWebImage to display data in the cell
-     */
-    func setToHandleImageURLStringsUsingSDWebImage(){
-        
-        self.handleLocalImageNameStrings = false
-        self.handleImageURLStrings = false
-        self.handleImageURLStringsUsingSDWebImage = true
-        
-    }
-
-    
-    /**
-     This method sets the MILHorizontalCollectionView to handle local image name strings with built in mechanisms to display data in the cell
-     */
-    func setToHandleLocalImageNameStrings(){
-        
-        self.handleLocalImageNameStrings = true
-        self.handleImageURLStrings = false
-        self.handleImageURLStringsUsingSDWebImage = false
-        
-    }
-    
-    
-    // MARK: UICollectionViewDataSource
-
-    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        //#warning Incomplete method implementation -- Return the number of sections
-        return 1
-    }
-
-
-    /**
-    This method defines the number of items in a section
-    */
-    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //#warning Incomplete method implementation -- Return the number of items in the section
-        return dataArray.count
-    }
-
-
-    /**
-    This method generates the cell for item at indexPath
-    
-    - parameter collectionView:
-    - parameter indexPath:
-    
-    - returns:
-    */
-    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("horizontalcell", forIndexPath: indexPath) as! MILHorizontalCollectionViewCell
-        
-        //1. Setup cell with image from image url (download image asynchronously and cache image)
-        if(handleImageURLStrings == true){
-            return self.setUpCellWithImageURL(cell, indexPath: indexPath)
-        }
-        //2. Setup cell with image from image url (Using SDWebImage framework to download image asynchronously and cache image)
-        else if(handleImageURLStringsUsingSDWebImage == true){
-            return self.setUpCellWithImageURLUsingSDWebImage(cell, indexPath: indexPath)
-        }
-            
-        //3. Setup cell with locally stored images
-        else{
-            return self.setUpCellWithLocallyStoredImage(cell, indexPath: indexPath)
-        }
-
-    }
-    
-    
-    
-    /**
-    This method sets up the MILHorizontalCollectionViewCell with a locally stored image with respect to indexPath.row in the dataArray. This method can be used when it is assumed that the strings the data array holds represent the names of locally stored images in Images.xcassets
-    
-    - parameter cell:
-    - parameter indexPath:
-    
-    - returns: MILHorizontalCollectionViewCell
-    */
     private func setUpCellWithLocallyStoredImage(cell : MILHorizontalCollectionViewCell, indexPath : NSIndexPath) -> MILHorizontalCollectionViewCell {
         
         let imageString : String = dataArray[indexPath.row] as String
@@ -177,16 +133,15 @@ class MILHorizontalCollectionViewController: UICollectionViewController {
         
         return cell
     }
-    
-    
+
     /**
-    This method sets up the MILHorizontalCollectionViewCell with an image from a url. It first will set up the cell with a placeholder image. Then it will check the cache to see if the image has already been downloaded and cached. If the cache has the image, it sets the cell with the image. If not, then it will asynchronously download the image, cache it, and then set the cell with this image.
-    
-    - parameter cell:
-    - parameter indexPath:
-    
-    - returns: MILHorizontalCollectionViewCell
-    */
+     This method sets up the MILHorizontalCollectionViewCell with an image from a url. It first will set up the cell with a placeholder image. Then it will check the cache to see if the image has already been downloaded and cached. If the cache has the image, it sets the cell with the image. If not, then it will asynchronously download the image, cache it, and then set the cell with this image.
+     
+     - parameter cell:
+     - parameter indexPath:
+     
+     - returns: MILHorizontalCollectionViewCell
+     */
     private func setUpCellWithImageURL(cell : MILHorizontalCollectionViewCell, indexPath : NSIndexPath) -> MILHorizontalCollectionViewCell{
         
         //set the cell with temporary locally stored placeholder image
@@ -199,48 +154,46 @@ class MILHorizontalCollectionViewController: UICollectionViewController {
         if let image = self.imageCache[urlString] {
             cell.imageView.image = image
         }
-        //cache doesn't contain image, asynchronously download image and then cache it, then set cell.
+            //cache doesn't contain image, asynchronously download image and then cache it, then set cell.
         else{
             self.asychronouslyDownloadImageFromURLAndSetCollectionViewCellAtIndexPath(urlString, indexPath: indexPath)
         }
         
         return cell
     }
-    
-    
+
     /**
-    This method sets up the MILHorizontalCollectionViewCell with an image from a url using the SDWebImage framework. The SDWebImage framework will out of the box asynchronously download the image, cache the image, and persist this cache accross multiple app instances.
-    
-    - parameter cell:
-    - parameter indexPath:
-    
-    - returns: MILHorizontalCollectionViewCell
-    */
+     This method sets up the MILHorizontalCollectionViewCell with an image from a url using the SDWebImage framework. The SDWebImage framework will out of the box asynchronously download the image, cache the image, and persist this cache accross multiple app instances.
+     
+     - parameter cell:
+     - parameter indexPath:
+     
+     - returns: MILHorizontalCollectionViewCell
+     */
     private func setUpCellWithImageURLUsingSDWebImage(cell : MILHorizontalCollectionViewCell, indexPath : NSIndexPath) -> MILHorizontalCollectionViewCell {
         
         
         //Code commented out since SDWebImage isn't imported in the project and to surpress errors and warnings, uncomment to use with sdWebImage
-       /* 
-        let urlString = self.dataArray[indexPath.row]
-        let url = NSURL(string: urlString)
-        
-        cell.imageView.sd_setImageWithURL(url, placeholderImage: UIImage(named: self.localPlaceHolderImageName))
-        */
+        /*
+         let urlString = self.dataArray[indexPath.row]
+         let url = NSURL(string: urlString)
+         
+         cell.imageView.sd_setImageWithURL(url, placeholderImage: UIImage(named: self.localPlaceHolderImageName))
+         */
         
         return cell
     }
-    
-    
+
     /**
-    This method asychronously downloades an image from the specified urlString, caches this image, and then updates the the collection view cell with this image.
-    
-    - parameter collectionView:
-    - parameter indexPath:
-    */
+     This method asychronously downloades an image from the specified urlString, caches this image, and then updates the the collection view cell with this image.
+     
+     - parameter collectionView:
+     - parameter indexPath:
+     */
     private func asychronouslyDownloadImageFromURLAndSetCollectionViewCellAtIndexPath(urlString : String, indexPath : NSIndexPath){
         if(urlString != ""){
             if let url  = NSURL(string: urlString){
-        
+                
                 let request: NSURLRequest = NSURLRequest(URL: url)
                 
                 NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data, response, error) in
@@ -262,16 +215,60 @@ class MILHorizontalCollectionViewController: UICollectionViewController {
             }
         }
     }
+}
+
+// MARK: UICollectionViewDataSource
+extension MILHorizontalCollectionViewController {
+    
+    override public func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        //#warning Incomplete method implementation -- Return the number of sections
+        return 1
+    }
     
     /**
-    This method determines the action that is taken when an item is tapped.
+     This method defines the number of items in a section
+     */
+    override public func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //#warning Incomplete method implementation -- Return the number of items in the section
+        return dataArray.count
+    }
     
-    - parameter collectionView:
-    - parameter indexPath:
-    */
-    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
+    /**
+     This method generates the cell for item at indexPath
+     
+     - parameter collectionView:
+     - parameter indexPath:
+     
+     - returns:
+     */
+    override public func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("horizontalcell", forIndexPath: indexPath) as!
+        MILHorizontalCollectionViewCell
+        
+        //1. Setup cell with image from image url (download image asynchronously and cache image)
+        if(handleImageURLStrings == true){
+            return self.setUpCellWithImageURL(cell, indexPath: indexPath)
+        }
+            //2. Setup cell with image from image url (Using SDWebImage framework to download image asynchronously and cache image)
+        else if(handleImageURLStringsUsingSDWebImage == true){
+            return self.setUpCellWithImageURLUsingSDWebImage(cell, indexPath: indexPath)
+        }
+            
+            //3. Setup cell with locally stored images
+        else{
+            return self.setUpCellWithLocallyStoredImage(cell, indexPath: indexPath)
+        }
+    }
+    
+    /**
+     This method determines the action that is taken when an item is tapped.
+     
+     - parameter collectionView:
+     - parameter indexPath:
+     */
+    override public func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
     {
         
     }
-
 }
